1545222ca3ff0019aebed3f4b760770d79297642
abpradeepa <abpradeepa3@gmail.com>
2019-12-05 09:09:47 -0800

    periodic

--- javatest1.java
+++ javatest1.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-               System.out.println("poojith");
-	       System.out.println("Hello, World , pradeepgowda");
+	       System.out.println("Hellowodpradeep");
 	          }
 		  }
 
